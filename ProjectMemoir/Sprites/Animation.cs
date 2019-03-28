2477c12dd5e6eb6b85e794245c0a46c960a41f12
Kurovah <45458052+Kurovah@users.noreply.github.com>
2019-03-28 10:06:16 +0000

    Added a Sprite and animation class

---
+++ ProjectMemoir/Sprites/Animation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectMemoir.Sprites
+{
+    public class Animation
+    {
+        Rectangle sourceRect, desRect;
+        Vector2 spriteSize, position;
+        int frames;
+        float delay = 15f;
+        Texture2D tex;
+        public Animation(Texture2D _tex, Vector2 _spritesize, Vector2 position, int _frameNo)
+        {
+            tex = _tex;
+            frames = _frameNo;
+            spriteSize = _spritesize;
+            sourceRect = new Rectangle(0,0, (int)spriteSize.X, (int)spriteSize.Y);
+            desRect = new Rectangle((int)position.X, (int)position.Y, (int)spriteSize.X, (int)spriteSize.Y);
+        }
+        public void Update(GameTime _gt)
+        {
+            desRect = new Rectangle((int)position.X, (int)position.Y, (int)spriteSize.X, (int)spriteSize.Y);
+        }
+
+        public void Draw(SpriteBatch _sb)
+        {
+            _sb.Draw(tex, desRect, sourceRect, Color.White);
+        }
+    }
+}
---
+++ ProjectMemoir/Sprites/Sprite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using ProjectMemoir.Sprites;
+
+namespace ProjectMemoir.Sprites
+{
+    public class Sprite
+    {
+        Animation anim;
+        public Sprite(ContentManager _con, Vector2 _pos)
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch _sb)
+        {
+            anim.Draw(_sb);
+        }
+    }
+}

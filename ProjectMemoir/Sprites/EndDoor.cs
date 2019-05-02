5bef5eb0509824d55d48901cfbbd75ea2c7c0fe8
Kurovah <45458052+Kurovah@users.noreply.github.com>
2019-05-02 19:15:37 +0100

    started on end door

---
+++ ProjectMemoir/Sprites/EndDoor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMemoir.Sprites
+{
+    public class EndDoor:Sprite
+    {
+        EndDoor() : base()
+        {
+
+        }
+    }
+}

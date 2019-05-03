85d633d712578962ab8809b3f9e8fed403caf14f
Omar Suliman <45458613+Atomiqs@users.noreply.github.com>
2019-05-03 11:48:11 +0100

    Rooms 10 & 11 done.

--- ProjectMemoir/Scenes/GameScenes/A11.cs
+++ ProjectMemoir/Scenes/GameScenes/A11.cs
@@ -24,21 +24,24 @@ namespace ProjectMemoir.Scenes
 
             background = con.Load<Texture2D>("backgrounds/DesVillageBK");
             //solids to collide with
-            newSolid(0,0,1,11);
-            newSolid(1, 0, 19, 1);
-            newSolid(0, 11, 19, 1);
-            newSolid(19, 1, 1, 11);
+            newSolid(0, 4, 1, 18);
+            newSolid(0, 0, 20, 1);
+            newSolid(4, 21, 16, 1);
+            newSolid(19, 1, 1, 20);
 
-            newSolid(1, 7, 4, 1);
-            newSolid(15, 7, 4, 1);
-            newSolid(7, 4, 6, 1);
-            newSolid(7, 10, 6, 1);
+            newSolid(1, 4, 15, 1);
+            newSolid(4, 8, 15, 1);
+            newSolid(1, 13, 15, 1);
+
 
             base.Load();
             at.tex = con.Load<Texture2D>("tilesets/hellscape");
             //add anything that uses the player as a target after this
-            newSceneChanger(0, -1, 1, 12, "A12", new Vector2(2, 9));
-            newSceneChanger(0, -1, 1, 12, "A10", new Vector2(2, 9));
+            newSceneChanger(-1, 22, 20, 1, "A12", new Vector2(1, 2));
+            newSceneChanger(-1, -1, 1, 12, "A10", new Vector2(18, 3));
+            newCharger(6, 19);
+            newProwler(7, 7);
+            newProwler(11, 12);
         }
 
         
--- ProjectMemoir/Scenes/GameScenes/A12.cs
+++ ProjectMemoir/Scenes/GameScenes/A12.cs
@@ -24,16 +24,19 @@ namespace ProjectMemoir.Scenes
 
             background = con.Load<Texture2D>("backgrounds/DesVillageBK");
             //solids to collide with
-            newSolid(0,0,1,11);
-            newSolid(1, 0, 19, 1);
+            newSolid(0, 0, 1, 9);
+            newSolid(4, 0, 16, 1);
             newSolid(0, 11, 19, 1);
             newSolid(19, 1, 1, 11);
+
+            newSolid(1, 6, 5, 1);
+            newSolid(14, 6, 5, 1);
 
             base.Load();
             at.tex = con.Load<Texture2D>("tilesets/hellscape");
             //add anything that uses the player as a target after this
-            newSceneChanger(0, -1, 1, 12, "A11", new Vector2(2, 9));
-            newSceneChanger(0, -1, 1, 12, "A13", new Vector2(2, 9));
+            newSceneChanger(-1, -1, 1, 20, "A13", new Vector2(18, 9));
+            newGriefTree(14, 1, "2");
         }
 
         

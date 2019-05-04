cc436ddb412b1f443c168dc59e166d138e2e40bd
Omar Suliman <45458613+Atomiqs@users.noreply.github.com>
2019-05-04 11:16:06 +0100

    Room 13 is complete, added side special pedestal to room 2 for room checking past room 12

--- ProjectMemoir/Scenes/GameScenes/A12.cs
+++ ProjectMemoir/Scenes/GameScenes/A12.cs
@@ -35,7 +35,7 @@ namespace ProjectMemoir.Scenes
             base.Load();
             at.tex = con.Load<Texture2D>("tilesets/hellscape");
             //add anything that uses the player as a target after this
-            newSceneChanger(-1, -1, 1, 20, "A13", new Vector2(18, 9));
+            newSceneChanger(-1, -1, 1, 20, "A13", new Vector2(38, 9));
             newGriefTree(14, 1, "2");
         }
 
--- ProjectMemoir/Scenes/GameScenes/A13.cs
+++ ProjectMemoir/Scenes/GameScenes/A13.cs
@@ -25,20 +25,37 @@ namespace ProjectMemoir.Scenes
             background = con.Load<Texture2D>("backgrounds/DesVillageBK");
             //solids to collide with
             newSolid(0, 0, 1, 9);
-            newSolid(1, 0, 19, 1);
-            newSolid(0, 11, 19, 1);
-            newSolid(19, 1, 1, 11);
+            newSolid(0, 11, 1, 6);
+            newSolid(1, 0, 40, 1);
+            newSolid(0, 17, 35, 1);
+            newSolid(40, 1, 1, 8);
+            newSolid(35, 11, 6, 7);
 
-            newSolid(1, 7, 4, 1);
-            newSolid(15, 7, 4, 1);
-            newSolid(7, 4, 6, 1);
-            newSolid(7, 10, 6, 1);
+            newSolid(38, 8, 2, 1);
+            newSolid(8, 6, 27, 1);
+            newSolid(1, 1, 5, 8);
+            newSolid(5, 9, 27, 1);
+
+            newSolid(35, 5, 1, 6);
+            newSolid(21, 5, 1, 1);
+            newSolid(8, 5, 1, 1);
+
+            newSolid(1, 14, 2, 3);
+            newSolid(10, 14, 3, 3);
+            newSolid(21, 14, 3, 3);
+            newSolid(32, 14, 3, 3);
+
 
             base.Load();
             at.tex = con.Load<Texture2D>("tilesets/hellscape");
             //add anything that uses the player as a target after this
-            newSceneChanger(0, -1, 1, 12, "A12", new Vector2(2, 9));
-            newSceneChanger(0, -1, 1, 12, "A14", new Vector2(2, 9));
+            newSceneChanger(41, -1, 1, 20, "A12", new Vector2(1, 9));
+            newSceneChanger(-1, -1, 1, 20, "A14", new Vector2(18, 9));
+            newCharger(14, 5);
+            newCharger(27, 5);
+            newProwler(7, 16);
+            newProwler(16, 16);
+            newProwler(28, 16);
         }
 
         

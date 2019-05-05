29bcaac07bcfb3b68af1c40e843b1d3ccfa526f3
Omar Suliman <45458613+Atomiqs@users.noreply.github.com>
2019-05-05 08:29:59 +0100

    Changes to Room 7, and room 19 done

--- ProjectMemoir/Scenes/GameScenes/A07.cs
+++ ProjectMemoir/Scenes/GameScenes/A07.cs
@@ -29,8 +29,14 @@ namespace ProjectMemoir.Scenes
 
             newSolid(14, 4, 5, 1);
             newSolid(7, 9, 6, 1);
-            newSolid(1, 15, 6, 1);
-            newSolid(13, 15, 6, 1);
+            newSolid(1, 15, 6, 2);
+            newSolid(13, 15, 6, 2);
+            newSeal(7, 15, "Blue");
+            newSeal(8, 15, "Blue");
+            newSeal(9, 15, "Blue");
+            newSeal(10, 15, "Blue");
+            newSeal(11, 15, "Blue");
+            newSeal(12, 15, "Blue");
 
             newSolid(19, 4, 1, 15);
             newSolid(0, 21, 20, 1);
@@ -39,7 +45,7 @@ namespace ProjectMemoir.Scenes
             at.tex = con.Load<Texture2D>("tilesets/hellscape");
             //add anything that uses the player as a target after this
             newSceneChanger(-1, -1, 0, 33, "A2", new Vector2(18, 9));
-            newSceneChanger(21, -1, 0, 10, "A18", new Vector2(1, 9));
+            newSceneChanger(21, -1, 0, 10, "A19", new Vector2(1, 19));
             newSceneChanger(21, 10, 0, 10, "A8", new Vector2(1, 12));
         }
 
